1281edd88f14bb34a31288bb46efdb071d8bc734
oussama-benelhadj-djelloul <obenelhadjdjelloul1999@gmail.com>
2021-04-14 13:02:28 +0200

    Final

--- server.js
+++ server.js
@@ -1,1 +1,40 @@
-const express = require ('express');
+const express = require('express');
+const app = express();
+var request = require("request");
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+
+//home page
+app.get('/', function (req, res) {
+    //console.log('Express Start');
+    //res.send('hello Nodejs');
+    res.render('index')
+})
+
+//quran page
+app.get('/quran', function (req, res) {
+    res.render('quran')
+})
+
+app.get('/quran/verse', function (req, res) {
+
+    var options = {
+        method: 'GET',
+        url: 'https://api.quran.com/api/v4/quran/verses/uthmani',
+        body: '{}'
+    };
+
+    request(options, function (error, response, body) {
+        if (error) console.log('problem in API');
+
+        let verses = JSON.parse(body);
+
+        //console.log(verses);
+        res.render('verse', { verses: verses })
+    });
+})
+
+const server = app.listen('3000', function () {
+    console.log('The Server is Working');
+});
